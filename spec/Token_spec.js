85bc403dd18e8df27d4ccb74c08ad68368f3c816
Waters <h2oboi89@gmail.com>
2017-06-03 16:12:06 -0400

    Added Token.toString() test

--- spec/Token_spec.js
+++ spec/Token_spec.js
@@ -2,6 +2,12 @@
 
 describe('Token', () => {
   const Token = require('../src/Token');
+  const TokenType = require('../src/TokenType');
+
+  it('should toString nicely', () => {
+    expect(new Token(TokenType.BANG, '!', undefined, 1).toString())
+      .toEqual('BANG ! undefined');
+  });
 
   // TODO: test token
 });
