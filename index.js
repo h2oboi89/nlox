53f0fd9ac9a7ba456757e59ab9032b1ddb6048ef
Waters <h2oboi89@gmail.com>
2017-06-23 13:59:25 -0400

    Merged main and index

--- index.js
+++ index.js
@@ -1,6 +1,93 @@
 'use strict';
 
-const main = require('./src/main');
+const commandLineArgs = require('command-line-args');
+const commandLineUsage = require('command-line-usage');
+const fs = require('fs-extra');
+const path = require('path');
+const readline = require('readline');
+
+const LoxError = require('./src/LoxError');
+const options = require('./src/options');
+const Scanner = require('./src/Scanner');
+const Parser = require('./src/Parser');
+const Interpreter = require('./src/Interpreter');
+
+const scanner = new Scanner();
+const parser = new Parser();
+const interpreter = new Interpreter();
+
+function run(source) {
+  if(LoxError.hadError) {
+    process.exit(65);
+  }
+  else if (LoxError.hadRuntimeError) {
+    process.exit(70);
+  }
+  else {
+    const tokens = scanner.scanTokens(source);
+
+    const statements = parser.parse(tokens);
+
+    if (LoxError.hadError) {
+      return;
+    }
+
+    interpreter.interpret(statements);
+  }
+}
+
+function runPrompt() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  rl.prompt();
+
+  rl.on('line', (line) => {
+    run(line);
+
+    LoxError.reset();
+
+    rl.prompt();
+  }).on('close', () => {
+    console.log();
+    process.exit(0);
+  });
+}
+
+function runFile(file) {
+  fs.stat(file)
+    .then((stats) => {
+      // TODO: run all files in a directory?
+      if(stats.isDirectory()) {
+        throw new Error(`${file} is a directory, require a file`);
+      }
+    })
+    .then(() => fs.readFile(file, 'utf8'))
+    .then((contents) => run(contents))
+    .catch((error) => {
+      console.error(error.message);
+      process.exit(1);
+    });
+}
+
+function main() {
+  const args = commandLineArgs(options.options);
+  const usage = commandLineUsage(options.sections);
+
+  if(args.help) {
+    return console.log(usage);
+  }
+
+  if(args.input) {
+    return runFile(path.join(process.cwd(), args.input));
+  }
+
+  return runPrompt();
+}
+
+module.exports = main;
 
 if(require.main === module) {
   main();
