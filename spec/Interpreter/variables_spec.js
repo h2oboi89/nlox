24fc6c8c16779b34842d2a6303d60a5a0ad78332
Waters <h2oboi89@gmail.com>
2017-07-04 16:41:18 -0400

    Added test for undefined variable

--- spec/Interpreter/variables_spec.js
+++ spec/Interpreter/variables_spec.js
@@ -4,23 +4,26 @@ describe('Interpreter - variables', () => {
   const common = require('./common');
   const interpret = common.interpret;
   const interpretAndPrint = common.interpretAndPrint;
+  const interpretToError = common.interpretToError;
 
   beforeAll(() => common.beforeAll());
   afterAll(() => common.afterAll());
 
-  describe('variables', () => {
-    it('var a; a; a = 1; a;', () => {
-      interpret('var a;');
-      interpretAndPrint('a;', 'nil');
-      interpretAndPrint('a = 1;', '1');
-      interpretAndPrint('a;', '1');
-    });
+  it('var a; a; a = 1; a;', () => {
+    interpret('var a;');
+    interpretAndPrint('a;', 'nil');
+    interpretAndPrint('a = 1;', '1');
+    interpretAndPrint('a;', '1');
+  });
 
-    it('var b = 2; b; b = 3; b;', () => {
-      interpret('var b = 2;');
-      interpretAndPrint('b;', '2');
-      interpretAndPrint('b = 3;', '3');
-      interpretAndPrint('b;', '3');
-    });
+  it('var b = 2; b; b = 3; b;', () => {
+    interpret('var b = 2;');
+    interpretAndPrint('b;', '2');
+    interpretAndPrint('b = 3;', '3');
+    interpretAndPrint('b;', '3');
+  });
+
+  it('undefined variable', () => {
+    interpretToError('c;', `Undefined variable 'c'.`);
   });
 });
